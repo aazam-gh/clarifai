96d60a6f92ae6cf6c1b957052cb71a772b75c628
Alcadeus <aazamthakur@gmail.com>
2024-01-22 11:43:30 +0530

    changed file name

--- app.py
+++ app.py
@@ -10,18 +10,14 @@ from moviepy.editor import AudioFileClip
 from moviepy.editor import vfx
 
 
-
 load_dotenv()
 clarifai_pat = os.getenv('CLARIFAI_PAT')
 
 
-
-
 def generate_prompts(user_prompt):
         
-    system_prompt = "You are an experienced social media growth and content maker. Your job is to make a short form video of 30 secs long based on the user's prompt. To do this you will generate 5 detailed prompts for feeding into DALLE to generate the images for the video and 1 tts prompt explaining the entire video with the help of the generated images. Return only the prompts as JSON response. The JSON should be in a format of image_prompts:[prompt:text] and tts_prompt:text" 
+    system_prompt = "You are an experienced social media growth and content maker. Your job is to make a short form video of 30 secs long based on the user's prompt. To do this you will generate 8 detailed prompts for feeding into DALLE to generate the images for the video and 1 tts prompt explaining the entire video with the help of the generated images. Return only the prompts as JSON response. The JSON should be in a format of image_prompts:[prompt:text] and tts_prompt:text" 
     inference_params = dict(temperature=0.9 ,top_p=1,  max_tokens=2048, system_prompt=system_prompt)
-
     # Model Predict
     model_prediction = Model("https://clarifai.com/openai/chat-completion/models/gpt-4-turbo").predict_by_bytes(user_prompt.encode(), input_type="text", inference_params=inference_params)
     testin = model_prediction.outputs[0].data.text.raw
@@ -60,40 +56,49 @@ def create_video(image_filenames, fps):
     clip.write_videofile("output.mp4", codec="libx264")
 
 
+def understand_file(uploaded_file):
+
+
+    
+    pass
 
 def generate_tts(prompt):
     inference_params = dict(voice="alloy", speed=1.0)
-    model_prediction = Model("https://clarifai.com/openai/tts/models/openai-tts-1").predict_by_bytes(prompt.encode(), input_type="text", inference_params=inference_params)
+    model_prediction = Model("https://clarifai.com/openai/tts/models/openai-tts-1-hd").predict_by_bytes(prompt.encode(), input_type="text", inference_params=inference_params)
     output_base64 = model_prediction.outputs[0].data.audio.base64   
     with open("audio.mp3", "wb") as f:
         f.write(output_base64)
     return output_base64
 
 
-st.title("AI Image Generator")
+def main(user_prompt):
+    prompts = generate_prompts(user_prompt)
+    image_filenames = []
+    testing = generate_tts(prompts["tts_prompt"])
+
+    for i in range(len(prompts["image_prompts"])):
+        filename = generate_image(prompts["image_prompts"][i]["prompt"], i)
+        image_filenames.append(filename)
+
+    create_video(image_filenames, fps=0.5)
+    for file_name in image_filenames:
+        os.remove(file_name)
+    os.remove("audio.mp3")
+
+    video_file = open('output.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+
+
+st.title("AI Reels Generator")
 rad=st.sidebar.radio("Navigation",["Home","About Us"])
 if rad=="Home":
     user_prompt = st.text_input("Enter the description of the video you want", "    ")
 
     # Generate image button
-    if st.button("Generate Image"):
+    if st.button("Generate Video from prompt"):
         try:
-            prompts = generate_prompts(user_prompt)
-            image_filenames = []
-            testing = generate_tts(prompts["tts_prompt"])
-
-            for i in range(len(prompts["image_prompts"])):
-                filename = generate_image(prompts["image_prompts"][i]["prompt"], i)
-                image_filenames.append(filename)
-    
-            create_video(image_filenames, fps=0.5)
-            for file_name in image_filenames:
-                os.remove(file_name)
-            os.remove("audio.mp3")
-
-            video_file = open('output.mp4', 'rb')
-            video_bytes = video_file.read()
-            st.video(video_bytes)
+            main(user_prompt)
         
             with open("output.mp4", "rb") as file:
                 btn = st.download_button(
@@ -106,9 +111,6 @@ if rad=="Home":
         except Exception as e:
             st.error(f"Error: {e}")
 
-    text=st.file_uploader("upload a file")
-    st.text(text)
-   
-
-    if 'generated_image.png' in os.listdir():
-        st.button("[Download Generated Image](generated_image.png)")
+    uploaded_file=st.file_uploader("Upload a file to use it as the video script")
+    if st.button("Generate Video from file"):
+        bytes_data = uploaded_file.getvalue()
